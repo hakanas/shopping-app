8f5b2948f86cad18bb095815a2d61734de487aaf
Hakan Aslan <hakanaslan90@gmail.com>
2021-01-08 22:09:39 +0300

    frontend project for product page

---
+++ web/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import VueAxios from "vue-axios";
+import axios from 'axios'
+import {BASE_URL} from "@/common/config";
+
+
+axios.defaults.baseURL = BASE_URL;
+createApp(App).use(VueAxios, axios).mount('#app')
